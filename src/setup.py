56f58125fbd8a9ecd9c7b5f461015c208354dcf8
Marek Wiewiorski <mwiewiorski@opera.com>
2015-10-16 16:23:32 +0200

    Initial commit

---
+++ src/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name="sccp",
+    version="0.5.0",
+    author="Marek Wiewiorski",
+    author_email="mwicat@gmail.com",
+    description=("Skinny protocol parser"),
+    license="GPLv3",
+    packages=find_packages()
+)
